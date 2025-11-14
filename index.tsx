1401e3c8e3b38d6bce8051a9b110f517f2340a61
Eric Niu <niuyuping@ydcrm.cn>
2025-11-14 14:23:59 +0900

    Fix Tailwind CSS bugs Add dockerfile

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
+import './styles/globals.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
